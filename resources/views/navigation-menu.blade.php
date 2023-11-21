34fd87a03fa5f58f71b1f5394a07cba4dfc8a6c6
Nathaniel C <nathanielcandra2003@gmail.com>
2023-11-21 12:23:16 +0700

    Hilangin dashboard

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,11 +12,11 @@
                     </div>
 
                     <!-- Navigation Links -->
-                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    {{-- <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                         <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                             {{ __('Dashboard') }}
                         </x-nav-link>
-                    </div>
+                    </div> --}}
 
                     <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                         <x-nav-link href="{{ route('question') }}" :active="request()->routeIs('question')">
@@ -221,20 +221,20 @@
 
         <!-- Responsive Navigation Menu -->
         <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
-            <div class="pt-2 pb-3 space-y-1">
+            {{-- <div class="pt-2 pb-3 space-y-1">
                 <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                     {{ __('Dashboard') }}
                 </x-responsive-nav-link>
-            </div>
+            </div> --}}
 
             <div class="pt-2 pb-3 space-y-1">
-                <x-responsive-nav-link href="{{ route('questions.index') }}" :active="request()->routeIs('dashboard')">
+                <x-responsive-nav-link href="{{ route('questions.index') }}" :active="request()->routeIs('question.index')">
                     {{ __('Question') }}
                 </x-responsive-nav-link>
             </div>
 
             <div class="pt-2 pb-3 space-y-1">
-                <x-responsive-nav-link href="{{ route('product') }}" :active="request()->routeIs('dashboard')">
+                <x-responsive-nav-link href="{{ route('product') }}" :active="request()->routeIs('product')">
                     {{ __('Purchase Token') }}
                 </x-responsive-nav-link>
             </div>
